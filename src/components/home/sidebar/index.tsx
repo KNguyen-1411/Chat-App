29d81fe95fb43ffc473fe94276b1d4874f98b683
Khánh Nguyên <147232444+KNguyen-1411@users.noreply.github.com>
2024-09-21 22:45:23 +0700

    [FIX] Fix icon zindex

--- src/components/home/sidebar/index.tsx
+++ src/components/home/sidebar/index.tsx
@@ -73,6 +73,7 @@ export default function Sidebar() {
                             scale: 2,
                             right: -10,
                             top: 300,
+                            zIndex:1000,
                         }}
                     >
                         <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm5.5 10a.5.5 0 0 0 .832.374l4.5-4a.5.5 0 0 0 0-.748l-4.5-4A.5.5 0 0 0 5.5 4z" />
